a8e4d81f4ec6618e18b8843504c814b8af125f92
yu hyun ju <67223214+hyeonju0121@users.noreply.github.com>
2023-08-29 23:47:34 +0900

    Feat : Member Entity & Repository 생성
    - 임시 회원 초기 데이터 생성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,12 @@ values (2, 'FREE_SAVINGS');
 
 insert into production_category(id, production_type)
 values (3, 'INSTALMENT_SAVINGS');
+
+insert into member(id, member_id, password, simple_password, name, email, created_at, updated_at)
+values (1, 'hyeonju0121', 'guswn123456789', '123456', '유현주', 'a01085249773@gmail.com', now(), now());
+
+insert into member(id, member_id, password, simple_password, name, email, created_at, updated_at)
+values (2, 'chichilove0621', 'ilovepet123', '180621', '치치', 'chichi0621@gmail.com', now(), now());
+
+insert into member(id, member_id, password, simple_password, name, email, created_at, updated_at)
+values (3, 'zizh121', 'zhun9317@!', '567890','김지훈', 'zizh121@naver.com', now(), now());
